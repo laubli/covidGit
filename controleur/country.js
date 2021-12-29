619ffacf23b4228a2e80e51f354b1232692e4585
Jeremie SAINGRE <jeremie.saingre@etu.univ-nantes.fr>
2021-12-29 15:52:29 +0100

    New file added: Country.js

---
+++ controleur/country.js
@@ -0,0 +1,5 @@
+/*
+Nous avons beaucoup de donnees provenent d'APIs. Chaque pays possède ainsi beaucoup de données qui lui est propre.
+Au lieu de travailler avec plusieurs listes, pourquoi ne pas travailler avec une classe "Country"?
+Cette classe suivra le pattern POJO (Plain Old JavaScript Object).
+*/

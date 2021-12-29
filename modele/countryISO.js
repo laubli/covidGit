56921e7d35bae81cf8ea7420ee39d6356a23fb27
Jeremie SAINGRE <jeremie.saingre@etu.univ-nantes.fr>
2021-12-29 23:55:54 +0100

    Feature: Get the name, slug name and ISO2 of X countries (modele -> countryISO.js)

--- modele/countryISO.js
+++ modele/countryISO.js
@@ -1,12 +1,13 @@
-const UrlApiIso = "../data/iso2And3.json";
+const UrlApiIso2To3 = "../data/iso2And3.json";
+const UrlApiFullNameToIso2 = "https://api.covid19api.com/countries";
 
 /**
  * Get the ISO3 code of a country based on it's ISO2 code and execute a method with this code
  * @param {*} countryISO2 ISO2 code of a  country (e.g. for France: 'FR')
  * @param {*} methodToExecute Execute the method with the given information. The information is a ISO3 code (e.g. for FR: 'FRA')
  */
-function getISO3(countryISO2, methodToExecute) {
-    fetch(UrlApiIso)
+export function getISO3(countryISO2, methodToExecute) {
+    fetch(UrlApiIso2To3)
     .then(response => response.json())
     .then(response => {
         methodToExecute(response[countryISO2]);
@@ -14,4 +15,35 @@ function getISO3(countryISO2, methodToExecute) {
     .catch(error => alert(error));
 }
 
-export {getISO3}
+/**
+ * Get randomly the name, slug name and ISO2 code of X countries
+ * @param {*} nbCountries Number of countries to get
+ * @param {*} methodToExecute Execute the method with the given information. The informations are X names, slug names and ISO2 code.
+ */
+export function getRandomsCountriesNameAndISO2(nbCountries, methodToExecute) {
+    fetch(UrlApiFullNameToIso2)
+    .then(response => response.json())
+    .then(response => {
+        var result = [];
+
+        // X countries are added to the result
+        while(result.length != nbCountries) {
+            // A random country is selected
+            var tpmCountry = response[Math.floor(Math.random() * response.length)];
+
+            // We check if it's already picked
+            var alreadyPicked = false;
+            result.forEach(country => {
+                if(country === tpmCountry) {
+                    alreadyPicked = true;
+                }
+            });
+
+            if(!alreadyPicked) {
+                result.push(tpmCountry);
+            }
+        }
+
+        methodToExecute(result);
+    });
+}

c9374e37e7123813e3923fac6f17c1a3e5e4a2c4
Jeremie SAINGRE <jeremie.saingre@etu.univ-nantes.fr>
2021-12-29 14:37:24 +0100

    Modification: Documentation added to "countryISO.js"

--- modele/countryISO.js
+++ modele/countryISO.js
@@ -1,5 +1,10 @@
 const UrlApiIso = "../data/iso2And3.json";
 
+/**
+ * Get the ISO3 code of a country based on it's ISO2 code and execute a method with this code
+ * @param {*} countryISO2 ISO2 code of a  country (e.g. for France: 'FR')
+ * @param {*} methodToExecute Execute the method with the given information. The information is a ISO3 code (e.g. for FR: 'FRA')
+ */
 function getISO3(countryISO2, methodToExecute) {
     fetch(UrlApiIso)
     .then(response => response.json())
